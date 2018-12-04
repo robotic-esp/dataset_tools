77ac7bbab527e7461c02693c1d2faa92bad6a6cc
Kevin Judd <kjudd@robots.ox.ac.uk>
2018-12-04 12:55:40 +0000

    Added calibration loader scripts.

---
+++ load_manufacturer_calibration.m
@@ -0,0 +1,42 @@
+function [camera_structs] = load_manufacturer_calibration(yaml_file)
+%   
+% Loads the manufacturer-proviced camera calibrations for the sensor
+% apparatus.
+%
+% NOTE: Depends on yamlmatlab which is freely available at
+%       https://code.google.com/archive/p/yamlmatlab/
+%
+% From: Kevin M. Judd and Jonathan D. Gammell, 
+%       The Oxford Multimotion Dataset: Multiple SE(3) Motions with Ground Truth
+%       kjudd@robots.ox.ac.uk, gammell@robots.ox.ac.uk
+%
+% input:
+%   yaml_file: calibration file path  
+%  
+% output:
+%   camera_structs: camera parameters
+%
+    if(~endsWith(yaml_file, 'manufacturer.yaml'))
+        warning('Are you sure this is a valid manufacturer.yaml file from the Oxford Multimotion Dataset?')
+    end
+    yaml_struct = ReadYaml(yaml_file);
+    
+    fields = fieldnames(yaml_struct);
+
+   
+    camera_structs(numel(fields),1).T_to_c0_from_c = eye(4);
+    for i = 1:numel(fields)
+        tmp = yaml_struct.(fields{i});
+        camera_structs(i).rostopic = tmp.rostopic;
+        camera_structs(i).resolution = tmp.resolution;
+        camera_structs(i).K = [tmp.intrinsics{1} 0 tmp.intrinsics{3};
+                               0 tmp.intrinsics{2} tmp.intrinsics{4};
+                               0 0 1];
+        if(isfield(tmp,'T_cn_cnm1'))
+            camera_structs(i).T_to_c0_from_c = invT(reshape(vertcat(tmp.T_cn_cnm1{:}),4,4) * camera_structs(i-1).T_to_c0_from_c);
+        else
+            camera_structs(i).T_to_c0_from_c = eye(4);
+        end
+        camera_structs(i).P = [camera_structs(i).K * camera_structs(i).T_to_c0_from_c(1:3,:); 0 0 0 1];
+    end
+end
---
+++ load_vicon_calibration.m
@@ -0,0 +1,21 @@
+function [T_to_apparatus_from_camera] = load_vicon_calibration(yaml_file)
+%   
+% Loads the calibrations transform from the camera to the vicon frame for
+% the sensor apparatus.
+%
+% From: Kevin M. Judd and Jonathan D. Gammell, 
+%       The Oxford Multimotion Dataset: Multiple SE(3) Motions with Ground Truth
+%       kjudd@robots.ox.ac.uk, gammell@robots.ox.ac.uk
+%
+% input:
+%   yaml_file: calibration file path  
+%  
+% output:
+%   T_A_from_B: transform from the camera to the vicon frame for the sensor apparatus
+%
+    if(~endsWith(yaml_file, 'vicon.yaml'))
+        warning('Are you sure this is a valid vicon.yaml file from the Oxford Multimotion Dataset?')
+    end
+    yaml_struct = ReadYaml(yaml_file);
+    T_to_apparatus_from_camera = reshape(vertcat(yaml_struct.T_apparatus_left{:}),4,4);
+end
