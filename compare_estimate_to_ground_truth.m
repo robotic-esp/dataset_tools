5ef25d4716759455fd1ab2f7a776a43b818c4c12
Kevin Judd <kjdd@robots.ox.ac.uk>
2021-11-11 14:58:48 -0500

    Cleaned up old unused parameters for compare_estimate_to_ground_truth

--- compare_estimate_to_ground_truth.m
+++ compare_estimate_to_ground_truth.m
@@ -1,4 +1,26 @@
-function [max_xyz_error, max_xyz_percent_error, max_rpy_error, avg_xyz_error, avg_rpy_error] = compare_estimate_to_ground_truth(trajectory_estimate, trajectory_gt, body_frame, path_label, as_percentage, T_estimate_from_gt)
+function [max_xyz_error, max_xyz_percent_error, max_rpy_error] = compare_estimate_to_ground_truth(trajectory_estimate, trajectory_gt, T_estimate_from_gt)
+%   
+% Compares estimated trajectory to the ground-truth trajectory and plots the xyz
+% and roll-pitch-yaw error in the ground-truth frame. The estimated trajectory
+% is calibrated to the ground-truth frame via the T_estimate_from_gt transform.
+%
+% From: Kevin M. Judd and Jonathan D. Gammell, 
+%       The Oxford Multimotion Dataset: Multiple SE(3) Motions with Ground Truth
+%       kjudd@robots.ox.ac.uk, gammell@robots.ox.ac.uk
+%
+% input:
+%   trajectory_estimate: a Kx1 cell array of 4x4 transformation matrices
+%   representing a estimated motion trajectory
+%   trajectory_gt: a Kx1 cell array of 4x4 transformation matrices
+%   representing the gorund-truth motion trajectory
+%   T_estimate_from_gt: a 4x4 transformation matrix representing the calibration
+%   from the ground-truth frame to the estimated frame
+%
+% output:
+%   max_xyz_error: the xyz error at the maximally displaced point along the trajectory
+%   max_xyz_percent_error: the max_xyz_error as a percentage of the total path at that point
+%   max_rpy_error: the rpy error at the maximally displaced point along the trajectory
+%
 
 if(length(trajectory_estimate) ~= length(trajectory_gt))
     error('Mismatched lengths');
@@ -7,27 +29,15 @@ end
 xyz_err = zeros(length(trajectory_estimate),3);
 rpy_err = zeros(length(trajectory_estimate),3);
 for i = 1:length(trajectory_estimate)
-    if(body_frame)
-        T_err = invT(trajectory_estimate{i}) * T_estimate_from_gt * trajectory_gt{i} * invT(T_estimate_from_gt);
-    else
-        T_err = invT(T_estimate_from_gt) * trajectory_estimate{i} * T_estimate_from_gt * invT(trajectory_gt{i});
-    end
+
+    T_err = invT(T_estimate_from_gt) * trajectory_estimate{i} * T_estimate_from_gt * invT(trajectory_gt{i});
     
     tmp = get_r_j_from_i_in_i_FROM_T_ji(T_err);
 	xyz_err(i,:) = tmp(1:3);
     rpy_err(i,:) = rotmat_to_rpy(T_err(1:3,1:3))';
 end
 
-if(path_label)
-    path = total_path_length(trajectory_gt);
-else
-    path = 1:length(trajectory_estimate);
-end
-
-if(as_percentage)
-    tmp = total_path_length(trajectory_gt);
-    xyz_err(2:end,:) = xyz_err(2:end,:) ./ tmp(2:end,:) * 100;
-end
+path = total_path_length(trajectory_gt);
 
 [max_xyz_error, i] = max(sqrt(sum(xyz_err.^2,2)));
 max_xyz_percent_error = max_xyz_error / path(i);
@@ -36,19 +46,12 @@ max_rpy_error = rpy_err(i,:)*180/pi;
 avg_xyz_error = mean(sqrt(sum(xyz_err.^2,2)));
 avg_rpy_error = mean(sqrt(sum(rpy_err.^2,2)));
 
-% figure;
+figure;
 subplot(2,1,1);
-co = [1 0 0; 0 1 0; 0 0 1];
-old_co = get(groot, 'defaultAxesColorOrder');
-set(groot,'defaultAxesColorOrder',co);
 hold on
 plot(path, xyz_err)
 set(gca, 'Box', 'off');
-if(as_percentage)
-    ylabel('Tran. Error (% total path)')
-else
-    ylabel('Tran. Error (m)')
-end
+ylabel('Tran. Error (m)')
 legend({'x','y','z'}, 'Location', 'Southwest', 'Orientation','horizontal');
 box off
 xticklabels([])
@@ -57,10 +60,8 @@ xlim([0, path(end)]);
 
 subplot(2,1,2);
 hold on
-set(groot,'defaultAxesColorOrder',co);
 plot(path, rpy_err)
 ylabel('Rot. Error (rad)')
-set(groot,'defaultAxesColorOrder',co);
 ylimits_rad = ylim;
 ylim(ylimits_rad);
 hold on
@@ -80,7 +81,6 @@ else
 end
 xlim([0, path(end)]);
 
-set(groot, 'defaultAxesColorOrder', old_co);
 linkaxes([subplot(2,1,1),subplot(2,1,2)], 'x');
 
 end
