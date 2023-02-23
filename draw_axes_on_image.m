c2b6659e69885ece8bcc6294e115b688a2d431f9
Jonathan Gammell <gammell@gmail.com>
2023-02-23 09:57:35 -0500

    Clean up kalibr/manufacturer calibration use.

--- draw_axes_on_image.m
+++ draw_axes_on_image.m
@@ -5,6 +5,8 @@
 %       The Oxford Multimotion Dataset: Multiple SE(3) Motions with Ground Truth
 %       kjudd@robots.ox.ac.uk, gammell@robots.ox.ac.uk
 
+clear;
+
 % User must set these paths
 path_to_vicon_data = ''; % path to vicon.csv file
 path_to_vicon_calibration = ''; % path to vicon.yaml calibration file
@@ -15,22 +17,31 @@ path_to_images = ''; % path to the directory containing the stereo RGB images
 
 [vicon_trajectories, vicon_objects, vicon_timestamps] = ingest_vicon_data(path_to_vicon_data);
 T_to_apparatus_from_left_camera = load_vicon_calibration(path_to_vicon_calibration);
-camera_structs_kalibr = load_kalibr_calibration(path_to_kalibr_calibration);
-% camera_structs_manufacturer = load_kalibr_calibration(path_to_manufacturer_calibration);
 timestamps = readtable([path_to_images '/stereo.csv']);
 timestamps = timestamps{:,2} + timestamps{:,3}/1e9; 
 
 
 % Projects axes into left stereo image frame. Other image frames can be
 % selected from the calibration.
-T_to_left_camera_from_current_camera = camera_structs_kalibr(1).T_to_c0_from_c;
+
+% camera_structs_kalibr = load_kalibr_calibration(path_to_kalibr_calibration);
+% T_to_left_camera_from_current_camera = camera_structs_kalibr(1).T_to_c0_from_c;
+% fu = camera_structs_kalibr(1).K(1,1);
+% fv = camera_structs_kalibr(1).K(2,2);
+% Cu = camera_structs_kalibr(1).K(1,3);
+% Cv = camera_structs_kalibr(1).K(2,3);
+% baseline = camera_structs_kalibr(1).K(2,2);
+
+camera_structs_manufacturer = load_manufacturer_calibration(path_to_manufacturer_calibration);
+T_to_left_camera_from_current_camera = camera_structs_manufacturer(2).T_to_c0_from_c;
+fu = camera_structs_manufacturer(2).K(1,1);
+fv = camera_structs_manufacturer(2).K(2,2);
+Cu = camera_structs_manufacturer(2).K(1,3);
+Cv = camera_structs_manufacturer(2).K(2,3);
+baseline = camera_structs_manufacturer(2).K(2,2);
+
 images_format = [path_to_images '/%06d_left.png']; 
 T_to_apparatus_from_current_camera = T_to_apparatus_from_left_camera * T_to_left_camera_from_current_camera;
-fu = camera_structs_kalibr(1).K(1,1);
-fv = camera_structs_kalibr(1).K(2,2);
-Cu = camera_structs_kalibr(1).K(1,3);
-Cv = camera_structs_kalibr(1).K(2,3);
-baseline = camera_structs_kalibr(1).K(2,2);
 P_uvd_to_xyz = [1/fu 0 0 -Cu/fu; ...
                 0 1/fv 0 -Cv/fv; ...
                 0 0 0 1; ...
@@ -39,13 +50,13 @@ P_xyz_to_uvd = inv(P_uvd_to_xyz);
 
 figure(1);
 for index = 1:1000
-    im = imread(sprintf(images_format, index));
     vicon_index = 1;
     while(vicon_timestamps(vicon_index) < timestamps(index+1))
         vicon_index = vicon_index + 1;
     end
 
-    clf;imshow(imread(sprintf(images_format, index)));
+    clf;
+    imshow(imread(sprintf(images_format, index)));
     for j = 1:length(vicon_objects)-1
         T_to_object_from_apparatus = invT(vicon_trajectories{j}{vicon_index}) * (vicon_trajectories{end}{vicon_index});
         T_to_object_from_camera = T_to_object_from_apparatus * T_to_apparatus_from_current_camera;
